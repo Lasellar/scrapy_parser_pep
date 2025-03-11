89e1a3571c9fc918ac9f96d9976d1d135533ce62
Maksim Polushin <btnchsky.sad@mail.ru>
2025-03-11 19:51:10 +0300

    review 2

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -9,11 +9,11 @@ from .settings import BASE_DIR, DATETIME_FORMAT
 class PepParsePipeline:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        RESULTS_DIR = BASE_DIR / 'results'
-        RESULTS_DIR.mkdir(exist_ok=True)
+        results_dir = BASE_DIR / 'results'
+        results_dir.mkdir(exist_ok=True)
         timestamp = dt.now().strftime(DATETIME_FORMAT)
         self.statuses = defaultdict(int)
-        self.file = open(RESULTS_DIR / f'pep_{timestamp}.csv', 'wb')
+        self.file = open(results_dir / f'pep_{timestamp}.csv', 'wb')
         self.exporter = CsvItemExporter(
             self.file, fields_to_export=['number', 'name', 'status']
         )
@@ -22,21 +22,25 @@ class PepParsePipeline:
         self.exporter.start_exporting()
 
     def process_item(self, item, spider):
-        self.statuses[item['status']] += 1
-        if not item.get('status'):
+        item_status = item.get('status')
+        if not item_status:
             raise KeyError
+        if not self.statuses.get(item_status):
+            self.statuses[item_status] = 1
+        else:
+            self.statuses[item.get('status')] += 1
         self.exporter.export_item(item)
         return item
 
     def close_spider(self, spider):
-        RESULTS_DIR = BASE_DIR / 'results'
+        results_dir = BASE_DIR / 'results'
         summary = [('Статус', 'Количество')]
         summary.extend(self.statuses.items())
         summary.append(('total', sum(self.statuses.values())))
-        RESULTS_DIR.mkdir(exist_ok=True)
+        results_dir.mkdir(exist_ok=True)
         now = dt.now().strftime(DATETIME_FORMAT)
         fname = f'status_summary_{now}.csv'
-        fpath = RESULTS_DIR / fname
+        fpath = results_dir / fname
         with open(fpath, 'w', encoding='utf-8') as file:
             writer = csv.writer(file, dialect='unix')
             writer.writerows(summary)
